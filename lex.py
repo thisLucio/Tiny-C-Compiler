725853c1e9e9c31516faa5fe5849ebdf7f6a58e2
thisLucio <luxusnietzsche@gmail.com>
2024-09-22 21:08:42 -0300

    Add new TokenType

--- lex.py
+++ lex.py
@@ -1,3 +1,5 @@
+import enum
+
 class Lexer:
     def __init__(self, source):
         self.source = source + '\n'
@@ -33,4 +35,59 @@ class Lexer:
 
     # Return the next token.
     def getToken(self):
-        pass
+        token = None
+
+        if self.curChar == '+':
+            token = Token(self.curChar, TokenType.PLUS)
+        elif self.curChar == '-':
+            token = Token(self.curChar, TokenType.MINUS)
+        elif self.curChar == '*':
+            token = Token(self.curChar, TokenType.ASTERISK)
+        elif self.curChar =='/':
+            token = Token(self.curChar, TokenType.SLASH)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\0':
+            token = Token(self.curChar, TokenType.EOF)
+        else:
+            pass
+
+        self.nextChar()
+        return token
+
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText
+        self.kind = tokenKind
+
+# TokenType is our enum for all the types of tokens.
+class TokenType(enum.Enum):
+	EOF = -1
+	NEWLINE = 0
+	NUMBER = 1
+	IDENT = 2
+	STRING = 3
+	# Keywords.
+	LABEL = 101
+	GOTO = 102
+	PRINT = 103
+	INPUT = 104
+	LET = 105
+	IF = 106
+	THEN = 107
+	ENDIF = 108
+	WHILE = 109
+	REPEAT = 110
+	ENDWHILE = 111
+	# Operators.
+	EQ = 201
+	PLUS = 202
+	MINUS = 203
+	ASTERISK = 204
+	SLASH = 205
+	EQEQ = 206
+	NOTEQ = 207
+	LT = 208
+	LTEQ = 209
+	GT = 210
+	GTEQ = 211
