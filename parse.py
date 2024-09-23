9d7c88f1d14ec156b0d0a0dc7954a64269f02ee5
thisLucio <luxusnietzsche@gmail.com>
2024-09-23 00:08:57 -0300

    new parser feat

--- parse.py
+++ parse.py
@@ -27,3 +27,45 @@ class Parser:
 
     def abort(self, message):
         sys.exit("Err> " + message)
+
+    # Production rules.
+
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+
+        # Parse all the stmt in the program.
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    # One of the following stmt's...
+    def statement(self):
+        # Check the first tokent to see what kind of stmt this is.
+
+        # "PRINT" (expression | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
+            self.nextToken()
+
+            if self.checkToken(TokenType.STRING):
+                # Simple string.
+                self.nextToken()
+            else:
+                # Expect an expression.
+                self.expression()
+        # "IF" comparison "THEN" {statement} "ENDIF"
+        elif self.checkToken(TokenType.IF):
+
+
+        # Newline.
+        self.nl()
+
+    # nl ::= '\n'+
+    def nl(self):
+         print("NEWLINE")
+
+         # Require at least one newline.
+         self.match(TokenType.NEWLINE)
+         # But we will allow extra newlines too, of course.
+         while self.checkToken(TokenType.NEWLINE):
+            self.nextToken()
